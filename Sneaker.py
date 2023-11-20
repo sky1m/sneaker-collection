923dd7f1c71f7de4b9d390089dd87c8acc157ed3
Skyler Muller <skyler.lmuller@gmail.com>
2023-11-20 15:05:16 -0600

    Created multiline comment on parameter data types

--- Sneaker.py
+++ Sneaker.py
@@ -1,2 +1,11 @@
 class Sneaker:
     def __init__ (self, brand, model, colorway, release_year, size, condition):
+         """
+        Parameters:
+        brand (str): Brand of the sneaker (e.g., Nike, Adidas).
+        model (str): Model of the sneaker (e.g., Air Jordan, Ultraboost).
+        colorway (str): Color scheme of the sneaker.
+        release_year (int): Year the sneaker was released.
+        size (float): Size of the sneaker.
+        condition (str): Condition of the sneaker (e.g., new, used).
+        """
